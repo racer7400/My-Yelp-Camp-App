08d750d76fda6f2d14a0c9240c93d2b1ed7f16a2
racer7400 <racer7400@gmail.com>
2017-07-06 08:57:04 -0400

    add dynamic pricing feature

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,7 @@ var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String,
+    cost: Number,
     location: String,
     lat: Number,
     lng: Number,
